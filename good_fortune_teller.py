2bf711ee9e8d09bdb8caa4e3ae46233453121f47
Nyx <cstark8@charlotte.edu>
2026-06-16 13:21:52 -0400

    initial commit: create bad_fortune_teller.py and good_fortune_teller.py

---
+++ good_fortune_teller.py
@@ -0,0 +1,14 @@
+"""
+Fortune Teller - Good Coding Practices Version
+
+Fortunes types: Career, love, health, life, lucky numbers, and lucky colors.
+"""
+
+
+def choose_fortune():
+
+def get_fortune():
+
+def get_lucky_numbers():
+
+def get_lucky_colors():
