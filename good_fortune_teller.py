9ccd7c0e5a2de62e6833ae8fecaa4664b8cdd386
Kaleiah.M <kaleiahmac@gmail.com>
2026-06-16 14:37:34 -0400

    Added initial broken code

--- good_fortune_teller.py
+++ good_fortune_teller.py
@@ -3,11 +3,17 @@ Fortune Teller - Good Coding Practices Version
 
 Fortunes types: Career, love, health, life, lucky numbers, and lucky colors.
 """
-
+import random
 
 def choose_fortune():
+    """
+        Takes a category string and returns a random fortune
+        from that specific category.
+        """
+
 
 def get_fortune():
+
 
 def get_lucky_numbers():
 
